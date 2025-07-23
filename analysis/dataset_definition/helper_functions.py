6c9c51853b3873b9a7168a91112381423023826d
CharlotteJames <charlotte.james@bristol.ac.uk>
2025-07-23 13:35:53 +0100

    update activity functions

--- analysis/dataset_definition/helper_functions.py
+++ analysis/dataset_definition/helper_functions.py
@@ -5,23 +5,24 @@ from ehrql.tables.tpp import (
     clinical_events, 
     medications, 
     ons_deaths,
-    emergency_care_attendances as eca
+    ec
 )
 
 
 def ed_attendances(start_date, end_date, where=True):
     return (
-        eca.where(where)
-        .where(eca.arrival_date.is_on_or_between(start_date, end_date))
-        .count_for_patient()
+        ec.where(where)
+        .where(ec.arrival_date.is_on_or_between(start_date, end_date))
+        .ec_ident        
+        .count_distinct_for_patient()
     )
-
 
 def primary_care_attendances(start_date, end_date, where=True):
     return (
         clinical_events.where(where)
         .where(clinical_events.date.is_on_or_between(start_date, end_date))
-        .count_for_patient()
+        .consultation_id
+        .count_distinct_for_patient()
     )
 
 
@@ -29,7 +30,8 @@ def hospital_admissions(start_date, end_date, where=True):
     return (
         apcs.where(where)
         .where(apcs.admission_date.is_on_or_between(start_date, end_date))
-        .count_for_patient()
+        .apcs_ident        
+        .count_distinct_for_patient()
     )
 
 def ever_matching_event_clinical_ctv3_before(codelist, start_date, where=True):
