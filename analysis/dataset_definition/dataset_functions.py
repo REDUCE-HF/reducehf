2ad7bbef0601a538c4587d00728e7d934a894093
CharlotteJames <charlotte.james@bristol.ac.uk>
2025-10-22 15:44:39 +0100

    tidy up

--- analysis/dataset_definition/dataset_functions.py
+++ analysis/dataset_definition/dataset_functions.py
@@ -441,25 +441,58 @@ def add_underserved(dataset, index_date, end_date, suffix='', iter=0):
     
     if iter==0:
         # Migrant status
-        dataset.migrant = last_matching_event_clinical_snomed_before(migrant, index_date, where=True).exists_for_patient()
+        dataset.migrant = last_matching_event_clinical_snomed_before(
+            migrant, 
+            index_date, 
+            where=True
+            ).exists_for_patient()
 
         # Non english speaking
-        dataset.non_english_speaking = last_matching_event_clinical_snomed_before(non_english_speaking, index_date, where=True).exists_for_patient()
+        dataset.non_english_speaking = last_matching_event_clinical_snomed_before(
+            non_english_speaking, 
+            index_date, 
+            where=True
+            ).exists_for_patient()
 
     # Substance abuse
-    substance_abuse_ = last_matching_event_clinical_snomed_between(substance_abuse, index_date - years(1), index_date, where=True).exists_for_patient()
+    substance_abuse_ = last_matching_event_clinical_snomed_between(
+        substance_abuse, 
+        index_date - years(1), 
+        index_date, 
+        where=True
+        ).exists_for_patient()
     dataset.add_column('substance_abuse' + suffix, substance_abuse_)
 
     # Homeless
-    homeless_ = last_matching_event_clinical_snomed_between(homeless, index_date - years(1), index_date, where=True).exists_for_patient()
+    homeless_ = last_matching_event_clinical_snomed_between(
+        homeless, 
+        index_date - years(1), 
+        index_date, where=True
+        ).exists_for_patient()
     dataset.add_column('homeless' + suffix, homeless_)
 
     # Housebound
-    housebound_date = last_matching_event_clinical_snomed_between(housebound, index_date - years(1), index_date, where=True).date
-    not_housebound_date = last_matching_event_clinical_snomed_between(no_longer_housebound, index_date - years(1), index_date, where=True).date
+    housebound_date = last_matching_event_clinical_snomed_between(
+        housebound, 
+        index_date - years(1), 
+        index_date, 
+        where=True
+        ).date
+
+    not_housebound_date = last_matching_event_clinical_snomed_between(
+        no_longer_housebound, 
+        index_date - years(1), 
+        index_date, 
+        where=True
+        ).date
+
     housebound_ = (
-        housebound_date.is_not_null()
-        & (housebound_date.is_after(not_housebound_date) | not_housebound_date.is_null())
+        housebound_date.is_not_null() & 
+        (housebound_date.is_after(
+            not_housebound_date
+            ) | 
+        not_housebound_date.is_null()
+        )
     )
     dataset.add_column('housebound' + suffix, housebound_)
 
