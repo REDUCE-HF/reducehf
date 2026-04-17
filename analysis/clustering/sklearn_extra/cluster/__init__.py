611d975bca3f3c24e74f9e3ba465f2539e45100b
CharlotteJames <charlotte.james@bristol.ac.uk>
2026-04-17 16:24:06 +0100

    edit sklearn_extra to run on OS

--- analysis/clustering/sklearn_extra/cluster/__init__.py
+++ analysis/clustering/sklearn_extra/cluster/__init__.py
@@ -1,4 +1,4 @@
 from ._k_medoids import KMedoids, CLARA
-from ._commonnn import commonnn, CommonNNClustering
+#from ._commonnn import commonnn, CommonNNClustering
 
 __all__ = ["KMedoids", "CLARA", "CommonNNClustering", "commonnn"]
